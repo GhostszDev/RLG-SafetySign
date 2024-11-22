417eb1920e55c6143690469572f8e0eab46ef298
Steven Rivera <ghostszmusic@gmail.com>
2024-11-22 10:27:10 -0600

    Update app.js
    update for the cors issues

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ var dataManagerRouter = require('./routes/dataManager');
 var app = express();
 
 app.use((req, res, next) => { 
-  res.header('Access-Control-Allow-Origin', 'http://localhost:63342'); 
+  res.header('Access-Control-Allow-Origin', '*'); 
   res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE'); 
   res.header('Access-Control-Allow-Headers', 'Content-Type'); 
   next(); });
