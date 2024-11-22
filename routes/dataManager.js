a246c4b8fa9f165e42b0ab13d931a7b67f91a29b
Steven Rivera <ghostszmusic@gmail.com>
2024-11-22 00:45:23 -0600

    Initial commit

---
+++ routes/dataManager.js
@@ -0,0 +1,44 @@
+var express = require('express');
+var router = express.Router();
+const fs = require('fs');
+
+// GET method route
+router.get('/', async (req, res) => {
+    await fs.readFile('JSON/data.JSON', 'utf8', (err, jsonData) => {
+
+        if (err) {
+            console.error('Error: ', err)
+        } else {
+            try {
+                const data = JSON.parse(jsonData);
+                console.log(jsonData);
+                res.send(data);
+            } catch (err) {
+                console.error('Error: ', err);
+            }
+        }
+    });
+
+
+})
+
+// POST method route
+router.post('/', async (req, res) => {
+
+    const newDate = {
+        date: req.body.date
+    }
+
+    await fs.writeFile('./JSON/data.JSON', JSON.stringify(newDate), (err) => {
+        if(err){
+            console.error('Error: ', err);
+        } else {
+            console.log('Successfully wrote to file!');
+            res.send({success: "Successfully wrote to file!"});
+        }
+    });
+})
+
+
+
+module.exports = router;
