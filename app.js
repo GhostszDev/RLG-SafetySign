6472fa1a6f589d517b6c9aa93179c0b6f09c8843
Steven Rivera <ghostszmusic@gmail.com>
2025-01-26 23:28:10 -0600

    updated test

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+const serverless = require('serverless-http');
 var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
@@ -47,4 +48,4 @@ app.use(function(err, req, res, next) {
 module.exports = {
   mode: 'production'
 };
-module.exports = app;
+module.exports = serverless(app);
