eb7b053ada056f289d126523b19392eae89b66fd
Steven Rivera <ghostszmusic@gmail.com>
2025-01-26 22:45:50 -0600

    adding netlify support testing

--- functions/server.js
+++ functions/server.js
@@ -1,10 +1,18 @@
 // functions/server.js
 const serverless = require('serverless-http');
 const express = require('express');
+const corsOptions = {
+    origin: "https://ghostszdev.github.io"
+}
+var dataManagerRouter = require('./routes/dataManager');
 const app = express();
+
+app.use(cors(corsOptions));
 
 app.get('/', (req, res) => {
     res.send('Hello, Netlify!');
 });
+
+app.use('/dataManager', dataManagerRouter);
 
 module.exports.handler = serverless(app);
